babd014385a179ac264ea84debf3cd4d3216f082
SriGobu <srigobu247@gmail.com>
2025-09-03 22:34:21 +0530

    WED WORK

--- lesson_11/11-TODO-LIST-MINI.js
+++ lesson_11/11-TODO-LIST-MINI.js
@@ -5,20 +5,37 @@ function renderTodoList () {
 let todoListHtml = '';
 
 for (let i = 0; i < todoList.length; i++) {
-  const todo = todoList[i];
-  const html = `<p>${todo}</p>`;
+  const todoObject = todoList[i];
+  // const name = todoObject.name;
+  // const dueDate = todoObject.dueDate;
+  const { name,dueDate } = todoObject; //{ name } - this takes the name out of the object and put it in a varibale with its name 
+  const html = `
+  <div>${name}</div>
+  <div>${dueDate}</div>
+  <button onclick="
+  todoList.splice(${i},1);
+  renderTodoList();
+  " class="delete-todo-button">Delete</button>
+   `;
   todoListHtml += html
 }
-console.log(todoListHtml);
 document.querySelector('.js-todo-list').innerHTML = todoListHtml;
 }
 
 function addTodo() {
   const inputElement = document.querySelector('.js-name-input');
-  const name = inputElement.value
+  const name = inputElement.value;
 
-  todoList.push(name);
-  console.log(todoList);
+  const dateInputElement = document.querySelector('.js-due-date-input');
+  const dueDate = dateInputElement.value;
+
+  todoList.push({
+    // name:name,
+    // dueDate:dueDate,
+    // shorthand for the above is below
+    name,
+    dueDate
+  });
 
   inputElement.value = '';
 
